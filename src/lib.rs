dfbc2da0b51d82173acfabd6f12088aa9a391664
Alex Davidson <coela@alxdavids.xyz>
2020-01-23 10:36:59 +0000

    Finish hkdf-rs tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,54 @@
 //!
 //! A wrapper around the rust-crypto implementation of HKDF
 //! [RFC5869](https://tools.ietf.org/html/rfc5869) locked to using SHA512 as the
-//! underlying hash function. The abstraction is necessary so that we can
-//! provide multiple sized output buffers, where the HKDF instance.
+//! underlying hash function. Focuses on splitting the `extract` and `expand`
+//! functionality.
 //!
 //! TODO: Rewrite to use the ring implementation. There were some difficulties
 //! around the way that ring does not give access to the raw bytes output by
 //! these algorithms
+///
+/// # Examples
+///
+/// Run using specific instantiation:
+/// ```
+/// use hkdf_sha512::Hkdf;
+/// use rand::rngs::OsRng;
+/// use rand_core::RngCore;
+///
+/// let hkdf = Hkdf{};
+///
+/// // extract bytes from random seed
+/// let mut rng = OsRng;
+/// let mut seed = vec![0; 32]; // length of seed determines security
+/// rng.fill_bytes(&mut seed);
+/// let mut out = Vec::new(); // output buffer will be resized by extract
+/// hkdf.extract(&seed, "some_secret_info".as_bytes(), &mut out); // out corresponds to raw PRK
+///
+/// // expand into output using raw PRK
+/// let expand_len = 70; // length of output buffer required
+/// let mut exp_out = vec![0; expand_len];
+/// hkdf.expand(&out, "some_info".as_bytes(), &mut exp_out);
+/// ```
+///
+/// Can also run using methods directly:
+/// ```
+/// use hkdf_sha512::{extract,expand};
+/// use rand::rngs::OsRng;
+/// use rand_core::RngCore;
+///
+/// // extract bytes from random seed
+/// let mut rng = OsRng;
+/// let mut seed = vec![0; 32]; // length of seed determines security
+/// rng.fill_bytes(&mut seed);
+/// let mut out = Vec::new(); // output buffer will be resized by extract
+/// extract(&seed, "some_secret_info".as_bytes(), &mut out); // out corresponds to raw PRK
+///
+/// // expand into output using raw PRK
+/// let expand_len = 70; // length of output buffer required
+/// let mut exp_out = vec![0; expand_len];
+/// expand(&out, "some_info".as_bytes(), &mut exp_out);
+/// ```
 
 use crypto::hkdf::{hkdf_extract,hkdf_expand};
 use crypto::sha2::Sha512;
@@ -18,17 +60,17 @@ const SHA512_OUTPUT_BYTES_LENGTH: usize = 64;
 /// A wrapper around the HKDF functionality in cases where it is required that
 /// you return a specific instance of HKDF.
 ///
-/// (largely redundant as we onyl support SHA-512)
+/// (largely redundant as we only support SHA-512)
 pub struct Hkdf{}
 
 impl Hkdf {
     /// extract for specific HKDF invocation
-    pub fn extract(seed: &[u8], secret: &[u8], out: &mut Vec<u8>) {
+    pub fn extract(&self, seed: &[u8], secret: &[u8], out: &mut Vec<u8>) {
         extract(seed, secret, out)
     }
 
     /// expand for specific HKDF invocation
-    pub fn expand(prk: &[u8], info: &[u8], out: &mut Vec<u8>) {
+    pub fn expand(&self, prk: &[u8], info: &[u8], out: &mut Vec<u8>) {
         expand(prk, info, out)
     }
 }
